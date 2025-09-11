f17453d78c3b849f51f8599d60d34a3d292f2b96
扫地僧 <upbyte@126.com>
2025-09-11 18:01:19 +0800

    chore: 更新 .gitignore并移除未使用的节点类型 - 在 .gitignore 文件中添加多个 .idea 目录，以忽略 IntelliJ IDEA 的项目配置文件 - 从 initNodes.ts 中移除未使用的 TextUpdaterNode 和 ColorSelectorNode 引用

--- upflow-web/src/pages/flow/initNodes.ts
+++ upflow-web/src/pages/flow/initNodes.ts
@@ -1,12 +1,8 @@
-import TextUpdaterNode from "@/components/TextUpdaterNode";
-import ColorSelectorNode from "@/components/ColorSelectorNode";
 import StartNode from "@/components/nodes/StartNode";
 import ConditionNode from "@/components/nodes/ConditionNode";
 import ScriptNode from "@/components/nodes/ScriptNode";
 
 export const nodeTypes = {
-    textUpdater: TextUpdaterNode,
-    selectorNode: ColorSelectorNode,
     startNode: StartNode,
     conditionNode: ConditionNode,
     scriptNode: ScriptNode,
